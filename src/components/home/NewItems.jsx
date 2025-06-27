e0b6b6a885fd8cf79d5513171f9633cacfc2a177
Tasha McGlashan <tmcglashan77@gmail.com>
2025-06-26 19:27:53 -0700

    updated NewItems with api, carousel, and timer

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,76 +1,134 @@
-import React from "react";
+// src/components/NewItems.jsx
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import Slider from "react-slick";
+import Countdown from "react-countdown";
+
+// Slick CSS (make sure these paths resolve in your build)
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const NewItems = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const res = await fetch(
+          "https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems"
+        );
+        const data = await res.json();
+        setItems(data);
+      } catch (err) {
+    
+      }
+    };
+    fetchItems();
+  }, []);
+
+  const settings = {
+    dots: false,
+    arrows: true,           
+    autoplay: true,         
+    autoplaySpeed: 3000,    
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      { breakpoint: 1024, settings: { slidesToShow: 3 } },
+      { breakpoint: 768,  settings: { slidesToShow: 2 } },
+      { breakpoint: 480,  settings: { slidesToShow: 1 } },
+    ],
+  };
+
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <div className="text-center">
-              <h2>New Items</h2>
-              <div className="small-border bg-color-2"></div>
-            </div>
-          </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link
-                    to="/author"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    title="Creator: Monica Lucas"
-                  >
-                    <img className="lazy" src={AuthorImage} alt="" />
-                    <i className="fa fa-check"></i>
-                  </Link>
-                </div>
-                <div className="de_countdown">5h 30m 32s</div>
+        <div className="text-center mb-4">
+          <h2>New Items</h2>
+          <div className="small-border bg-color-2"></div>
+        </div>
 
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
-                      </div>
-                    </div>
+        {items.length > 0 ? (
+          <Slider {...settings}>
+            {items.map((item) => (
+              <div key={item.id} className="px-2">
+                <div className="nft__item">
+                  <div className="author_list_pp">
+                    <Link
+                      to={`/author/${item.authorId}`}
+                      title={`Creator: ${item.authorName}`}
+                    >
+                      <img
+                        className="lazy"
+                        src={item.authorImage}
+                        alt={item.authorName}
+                      />
+                      <i className="fa fa-check"></i>
+                    </Link>
                   </div>
 
-                  <Link to="/item-details">
-                    <img
-                      src={nftImage}
-                      className="lazy nft__item_preview"
-                      alt=""
+                  <div className="de_countdown">
+                    <Countdown
+                      date={new Date(item.expiryDate)}
+                      renderer={({ hours, minutes, seconds, completed }) =>
+                        completed ? (
+                          <span>Ended</span>
+                        ) : (
+                          <span>
+                            {hours}h {minutes}m {seconds}s
+                          </span>
+                        )
+                      }
                     />
-                  </Link>
-                </div>
-                <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <div className="nft__item_price">3.08 ETH</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>69</span>
+                  </div>
+
+                  <div className="nft__item_wrap">
+                    <div className="nft__item_extra">
+                      <div className="nft__item_buttons">
+                        <button>Buy Now</button>
+                        <div className="nft__item_share">
+                          <h4>Share</h4>
+                          <a href="#" target="_blank" rel="noreferrer">
+                            <i className="fa fa-facebook fa-lg"></i>
+                          </a>
+                          <a href="#" target="_blank" rel="noreferrer">
+                            <i className="fa fa-twitter fa-lg"></i>
+                          </a>
+                          <a href="#">
+                            <i className="fa fa-envelope fa-lg"></i>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+
+                    <Link to={`/item-details/${item.id}`}>
+                      <img
+                        src={item.nftImage}
+                        className="lazy nft__item_preview"
+                        alt={item.nftName}
+                      />
+                    </Link>
+                  </div>
+
+                  <div className="nft__item_info">
+                    <Link to={`/item-details/${item.id}`}>
+                      <h4>{item.nftName}</h4>
+                    </Link>
+                    <div className="nft__item_price">{item.price} ETH</div>
+                    <div className="nft__item_like">
+                      <i className="fa fa-heart"></i>
+                      <span>{item.likes}</span>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </Slider>
+        ) : (
+          <p>Loading new items…</p>
+        )}
       </div>
     </section>
   );
